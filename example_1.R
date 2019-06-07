d27c59cbc5c49871aa455671bf119ae42eff8502
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-07 12:05:33 +0200

    Put results in subfolder

--- example_1.R
+++ example_1.R
@@ -7,9 +7,9 @@ suppressMessages(library(ggtree))
 library(beautier)
 
 #root_folder <- path.expand("~/GitHubs/pirouette_article")
+root_folder <- getwd()
 example_no <- 1
-#example_folder <- file.path(root_folder, paste0("example_", example_no))
-example_folder <- getwd()
+example_folder <- file.path(root_folder, paste0("example_", example_no))
 dir.create(example_folder, showWarnings = FALSE, recursive = TRUE)
 setwd(example_folder)
 set.seed(314)
